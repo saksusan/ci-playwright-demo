c91973b39a59a908059b9285af941bd062448859
usan <usan.sakhakarmi@vairavtech.com>
2026-02-24 16:57:06 +0545

    test file edit

--- qa-automation/tests/example.spec.js
+++ qa-automation/tests/example.spec.js
@@ -1,6 +1,26 @@
 const { test, expect } = require('@playwright/test');
+const { beforeEach } = require('node:test');
 
-test('Test Login Page Title', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/login');
-  await expect(page).toHaveTitle(/The Internet/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+});
+
+test('Test Home Page Title', async ({ page }) => {
+  await expect(page).toHaveTitle("QA Practice Store");
+});
+
+test('Test Valid Login', async ({ page }) => {
+  await page.locator('#username').fill("admin")
+  await page.locator('#password').fill("password123")
+  await page.locator('#loginBtn').click();
+  await expect(page.locator('#message')).toHaveText('Login Successful')
+});
+
+// test('Test Invalid Login', async ({ page }) => {
+//   await expect(page).toHaveTitle("QA Practice Store");
+// });
+
+test('Test Store', async ({ page }) => {
+  await page.locator('#addToCartBtn').click();
+  await expect(page.locator('#cartList')).toHaveText("Item added!");
 });
